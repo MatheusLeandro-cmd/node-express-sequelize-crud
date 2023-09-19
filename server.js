8e4f9dd8243e31e6887a167d469dfc89bceec57a
MatheusLeandro-cmd <monteiropoeta@gmail.com>
2023-09-19 09:11:30 -0300

    Subindo server.js com suas devidas alterações

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/", (req,res) => {
    res.json({message: 'Hello World!'}) 
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
     console.log(`Server funcionando na porta ${PORT}.`);
 });
